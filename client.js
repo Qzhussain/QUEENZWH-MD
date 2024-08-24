134c5ab24ef5e22acf3b641a509e1a1fe291b8fb
Qzhussain <zahraqhussain620@gmail.com>
2024-08-24 18:08:06 +0700

    Create client.js

---
+++ client.js
@@ -0,0 +1,16 @@
+|-- node_modules
+|-- public
+|   |-- scripts
+|       |-- handleEvents.js
+|       |-- handleUI.js
+|   |-- client.js
+|   |-- index.html
+|   |-- style.css
+|-- src
+|   |-- config.js
+|   |-- mapAlgorithm.js
+|   |-- server.js
+|-- .gitignore
+|-- package-lock.json
+|-- package.json
+|-- README.md
